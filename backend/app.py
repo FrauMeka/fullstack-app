20c59d3b72f76897c1b61a17112d724fae3bc04e
Meerim Sarbalieva <150505559+FrauMeka@users.noreply.github.com>
2026-04-30 11:36:54 +0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+import time
 import psycopg2
 
 app = Flask(__name__)
@@ -8,12 +9,22 @@ CORS(app)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+
 def get_conn():
-    return psycopg2.connect(DATABASE_URL)
+    for i in range(10):
+        try:
+            return psycopg2.connect(DATABASE_URL)
+        except psycopg2.OperationalError:
+            print("Waiting for PostgreSQL...")
+            time.sleep(3)
+
+    raise Exception("PostgreSQL is not ready")
+
 
 def init_db():
     conn = get_conn()
     cur = conn.cursor()
+
     cur.execute("""
         CREATE TABLE IF NOT EXISTS students (
             id SERIAL PRIMARY KEY,
@@ -21,35 +32,44 @@ def init_db():
             student_id TEXT NOT NULL
         )
     """)
+
     conn.commit()
     cur.close()
     conn.close()
+
 
 @app.route("/")
 def home():
     return "Backend is running"
 
+
 @app.route("/api/data", methods=["GET"])
 def get_data():
     conn = get_conn()
     cur = conn.cursor()
+
     cur.execute("SELECT id, name, student_id FROM students ORDER BY id")
     rows = cur.fetchall()
+
     cur.close()
     conn.close()
 
     data = []
+
     for row in rows:
         data.append({
             "id": row[0],
             "name": row[1],
             "student_id": row[2]
         })
+
     return jsonify(data)
+
 
 @app.route("/api/data", methods=["POST"])
 def add_data():
     data = request.json
+
     name = data.get("name")
     student_id = data.get("student_id")
 
@@ -58,11 +78,14 @@ def add_data():
 
     conn = get_conn()
     cur = conn.cursor()
+
     cur.execute(
         "INSERT INTO students (name, student_id) VALUES (%s, %s) RETURNING id",
         (name, student_id)
     )
+
     new_id = cur.fetchone()[0]
+
     conn.commit()
     cur.close()
     conn.close()
@@ -73,16 +96,20 @@ def add_data():
         "student_id": student_id
     }), 201
 
+
 @app.route("/api/data/<int:item_id>", methods=["DELETE"])
 def delete_data(item_id):
     conn = get_conn()
     cur = conn.cursor()
+
     cur.execute("DELETE FROM students WHERE id = %s", (item_id,))
+
     conn.commit()
     cur.close()
     conn.close()
 
     return jsonify({"message": "Deleted successfully"})
+
 
 if __name__ == "__main__":
     init_db()
